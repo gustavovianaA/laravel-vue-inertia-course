19714311e597a3de5d92958383a79b9a27341543
Gustavo Viana de Alencar <gustavow3dev@gmail.com>
2025-04-03 13:16:06 -0300

    End of section 7 - Adding Some Style

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,14 +1,29 @@
 <template>
-    <Link href="/listing">Listings </Link>&nbsp;
-    <Link href="/listing/create">New Listing </Link>
+    <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-900 w-full">
+        <div class="container mx-auto">
+            <nav class="p-4 flex items-center justify-between">
+                <div class="text-lg font-medium">
+                    <Link :href="route('listing.index')">Listings</Link>
+                </div>
+                <div class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center">
+                    <Link :href="route('listing.index')">LaraZillow</Link>
+                </div>
+                <div>
+                    <Link :href="route('listing.create')"
+                        class="btn-primary">+ New Listing
+                    </Link>
+                </div>
+            </nav>
+        </div>
+    </header>
 
-    <!-- <div>The page with time {{ timer }}</div> -->
-
-    <div v-if="flashSuccess" class="success">
-        {{ flashSuccess }}
-    </div>
-
-    <slot>Default</slot>
+    <main class="container mx-auto p-4">
+        <div v-if="flashSuccess"
+            class="mb-4 border rounded-md shadow-sm border-green-200 dark:border-green-800 bg-green-50 dark:bg-green-900 p-2">
+            {{ flashSuccess }}
+        </div>
+        <slot>Default</slot>
+    </main>
 </template>
 
 <script setup>
@@ -28,10 +43,3 @@ const flashSuccess = computed(
 //const timer = ref(0)
 //setInterval(() => timer.value++, 1000)
 </script>
-
-<style scoped>
-.success {
-    background-color: green;
-    color: white;
-}
-</style>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,14 +5,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Laravezillow</title>
     @routes
     @vite('resources/js/app.js')
     @inertiaHead
 
 </head>
 
-<body>
+<body class="bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-300">
     @inertia
 </body>
 </html>
